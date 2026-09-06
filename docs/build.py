14d6f51a5f734fe68183821379cb667c5e3416b2
tungsten <tungsten@tungsten-macmini.local>
2026-09-07 01:16:40 +0900

    0.9.2

--- docs/build.py
+++ docs/build.py
@@ -10,7 +10,7 @@ SITE = "https://ojtiger.github.io/pounce/"
 REPO = "https://github.com/ojtiger/pounce"
 LATEST = REPO + "/releases/latest"
 BREW = "brew install --cask ojtiger/tap/pounce"
-VERSION = "0.9.1"
+VERSION = "0.9.2"
 
 LANGS = {
   "ko": {
